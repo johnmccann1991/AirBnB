9afa7ecadda181495053e42e8d27d6e8f32a8e83
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-01-17 21:53:38 +0000

    practice #6

--- practice-6.sql
+++ practice-6.sql
@@ -14,5 +14,7 @@
 -- | Bridgeport             | 5559     |
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
-
+SELECT listings.neighborhood, COUNT (*)
+FROM reviews INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY listings.neighborhood;
 
